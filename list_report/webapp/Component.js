4a95c079102f2799b662c0aeaf230b0ed82abe92
JAYESH-U <uramanattijayesh@gmail.com>
2025-10-14 09:17:45 +0530

    List report template

---
+++ list_report/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("ibm.sap.ux.listreport.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
